8707bdbfac8c9ca6afdcdf2ae2c7a8cc3d203ec6
Amisha Singh <amishasinghrajput67@gmail.com>
2024-06-15 20:37:03 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "a55ba7b8d4785d11b10a4cac4201660f",
+  base: "http://api.openweathermap.org/data/2.5/",
+};
